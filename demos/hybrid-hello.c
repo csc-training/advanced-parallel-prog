d1c677ee915bab6cfb7264ec45d9dff222278ebc
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-02-05 10:26:52 +0200

    Hybrid hello world

---
+++ demos/hybrid-hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    int my_id, omp_rank;
+    int provided, required=MPI_THREAD_FUNNELED;
+    MPI_Init_thread(&argc, &argv, required,
+            &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+#pragma omp parallel private(omp_rank)
+    {
+        omp_rank = omp_get_thread_num();
+        printf("I'm thread %d in process %d\n",
+                omp_rank, my_id);
+    }
+    MPI_Finalize();
+}
