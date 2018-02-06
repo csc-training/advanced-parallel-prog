699dec758712c402b91d69008848195444e7e5be
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-02-06 16:16:27 +0200

    Simplify model solutions

--- hybrid/hello-world/solution/hybrid-hello.f90
+++ hybrid/hello-world/solution/hybrid-hello.f90
@@ -1,42 +1,29 @@
 program hello
-  use omp_lib
-  use mpi
-  implicit none
-  integer :: provided, my_id, ntask, tid, nthreads, msg, i, rc
+    use omp_lib
+    use mpi
+    implicit none
+    integer :: my_id, tid, rc
+    integer :: provided, required=MPI_THREAD_FUNNELED
 
-  call MPI_Init_thread(MPI_THREAD_MULTIPLE, provided, rc)
-  if (provided < MPI_THREAD_MULTIPLE) then
-     write(*,*) 'MPI version does not support MPI_THREAD_MULTIPLE'
-     call MPI_Abort(MPI_COMM_WORLD,11,rc)
-  end if
+    call MPI_Init_thread(required, provided, rc)
+    call MPI_Comm_rank(MPI_COMM_WORLD, my_id, rc)
 
-  call MPI_Comm_rank(MPI_COMM_WORLD, my_id, rc)
-  call MPI_Comm_size(MPI_COMM_WORLD, ntask, rc)
+    !$omp parallel private(tid)
+    tid = omp_get_thread_num()
+    write(*,'(A,I2,A,I2)') "I'm thread ", my_id, ' in process ', tid
+    !$omp end parallel
 
-  if (ntask < 2) then
-     write(*,*) 'Call this program using at least 2 tasks'
-     call MPI_Abort(MPI_COMM_WORLD, 5, rc)
-  end if
-
-!$omp parallel default(shared) private(msg, tid, nthreads, i)
-  nthreads = omp_get_num_threads()
-  tid = omp_get_thread_num()
-
-  if (my_id == 0) then
-     !$omp single
-     write(*,'(I3,A)') nthreads, ' threads in the master rank'
-     !$omp end single
-     do i = 1, ntask-1
-        call MPI_Send(tid, 1, MPI_INTEGER, i, tid, MPI_COMM_WORLD, rc)
-     end do
-  else
-     call MPI_Recv(msg, 1, MPI_INTEGER, 0, tid, MPI_COMM_WORLD, &
-          & MPI_STATUS_IGNORE, rc)
-     write(*,'(A,I3,A,I3,A,I3)') 'Rank ', my_id, ' thread ', tid, &
-          & ' received ', msg
-  end if
-!$omp end parallel
-
-  call MPI_Finalize(rc)
-
+    if (my_id == 0) then
+        write(*,*) ''
+        write(*,'(A,I1)') 'Provided thread support level: ', provided
+        write(*,'(A,I1,A)') '  ', MPI_THREAD_SINGLE, &
+                            & ' - MPI_THREAD_SINGLE'
+        write(*,'(A,I1,A)') '  ', MPI_THREAD_FUNNELED, &
+                            & ' - MPI_THREAD_FUNNELED'
+        write(*,'(A,I1,A)') '  ', MPI_THREAD_SERIALIZED, &
+                            & ' - MPI_THREAD_SERIALIZED'
+        write(*,'(A,I1,A)') '  ', MPI_THREAD_MULTIPLE, &
+                            & ' - MPI_THREAD_MULTIPLE'
+    end if
+    call MPI_Finalize(rc)
 end program hello
